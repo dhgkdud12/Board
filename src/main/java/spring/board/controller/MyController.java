0e1c6231ae07e19502c895581b8a4c608ec91a7a
hy <dhgkdud14@gmail.com>
2022-09-20 17:47:36 +0900

    Revert "Revert "로그인, 게시글, 댓글 기능""
    This reverts commit d915e3880ee889f79ca5a6915f137a119d7a8d87.

---
+++ src/main/java/spring/board/controller/MyController.java
@@ -0,0 +1,39 @@
+package spring.board.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.board.entity.Board;
+import spring.board.entity.Comment;
+import spring.board.service.BoardService;
+import spring.board.service.CommentService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping("/myPage")
+public class MyController {
+
+    private final BoardService boardService;
+    private final CommentService commentService;
+
+
+    public MyController(BoardService boardService, CommentService commentService) {
+        this.boardService = boardService;
+        this.commentService = commentService;
+    }
+
+    // 내가 작성한 글
+    @GetMapping("/board")
+    public List<Board> myBoards(HttpServletRequest request) {
+        return boardService.selectPostByUserId(request);
+    }
+    
+    // 내가 작성한 댓글
+    @GetMapping("/comment")
+    public List<Comment> myComments(HttpServletRequest request) {
+        return commentService.selectCommentsByUserId(request);
+    }
+
+}
---
+++ src/main/java/spring/board/dto/UserSessionDto.java
@@ -0,0 +1,15 @@
+package spring.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserSessionDto {
+    private Integer idx;
+    private String name;
+}
